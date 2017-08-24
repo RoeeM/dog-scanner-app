bfe68820a2f53f6cff31d608eb292767a754184d
RoeeM <roee.mayerwoicz@gmail.com>
2017-08-24 13:48:35 +0300

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,38 +1,91 @@
 import { Component, ViewChild } from '@angular/core';
-import { Nav, Platform } from 'ionic-angular';
+import { Platform, Nav, Config } from 'ionic-angular';
+
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
+import { CardsPage } from '../pages/cards/cards';
+import { ContentPage } from '../pages/content/content';
+import { FirstRunPage } from '../pages/pages';
+import { ListMasterPage } from '../pages/list-master/list-master';
+import { LoginPage } from '../pages/login/login';
+import { MapPage } from '../pages/map/map';
+import { MenuPage } from '../pages/menu/menu';
+import { SearchPage } from '../pages/search/search';
+import { SettingsPage } from '../pages/settings/settings';
+import { SignupPage } from '../pages/signup/signup';
+import { TabsPage } from '../pages/tabs/tabs';
+import { TutorialPage } from '../pages/tutorial/tutorial';
+import { WelcomePage } from '../pages/welcome/welcome';
+
+import { Settings } from '../providers/providers';
+
+import { TranslateService } from '@ngx-translate/core'
 
 @Component({
-  templateUrl: 'app.html'
+  template: `<ion-menu [content]="content">
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Pages</ion-title>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content>
+      <ion-list>
+        <button menuClose ion-item *ngFor="let p of pages" (click)="openPage(p)">
+          {{p.title}}
+        </button>
+      </ion-list>
+    </ion-content>
+
+  </ion-menu>
+  <ion-nav #content [root]="rootPage"></ion-nav>`
 })
 export class MyApp {
+  rootPage = FirstRunPage;
+
   @ViewChild(Nav) nav: Nav;
 
-  rootPage: any = HomePage;
+  pages: any[] = [
+    { title: 'Tutorial', component: TutorialPage },
+    { title: 'Welcome', component: WelcomePage },
+    { title: 'Tabs', component: TabsPage },
+    { title: 'Cards', component: CardsPage },
+    { title: 'Content', component: ContentPage },
+    { title: 'Login', component: LoginPage },
+    { title: 'Signup', component: SignupPage },
+    { title: 'Map', component: MapPage },
+    { title: 'Master Detail', component: ListMasterPage },
+    { title: 'Menu', component: MenuPage },
+    { title: 'Settings', component: SettingsPage },
+    { title: 'Search', component: SearchPage }
+  ]
 
-  pages: Array<{title: string, component: any}>;
-
-  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
-    this.initializeApp();
-
-    // used for an example of ngFor and navigation
-    this.pages = [
-      { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
-    ];
-
+  constructor(private translate: TranslateService, private platform: Platform, settings: Settings, private config: Config, private statusBar: StatusBar, private splashScreen: SplashScreen) {
+    this.initTranslate();
   }
 
-  initializeApp() {
+  ionViewDidLoad() {
     this.platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+    });
+  }
+
+  initTranslate() {
+    // Set the default language for translation strings, and the current language.
+    this.translate.setDefaultLang('en');
+
+    if (this.translate.getBrowserLang() !== undefined) {
+      this.translate.use(this.translate.getBrowserLang());
+    } else {
+      this.translate.use('en'); // Set your language here
+    }
+
+    this.translate.get(['BACK_BUTTON_TEXT']).subscribe(values => {
+      this.config.set('ios', 'backButtonText', values.BACK_BUTTON_TEXT);
     });
   }
 

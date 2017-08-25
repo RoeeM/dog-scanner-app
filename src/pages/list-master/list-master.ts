b25433f62c8bf3fc1f91c742ed32a896c7265839
Lidor Cohen <Lidor Cohen>
2017-08-25 07:36:58 +0300

    Add basic google map

--- src/pages/list-master/list-master.ts
+++ src/pages/list-master/list-master.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { NavController, ModalController } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { NavController, ModalController, Platform } from 'ionic-angular';
 
 import { ItemCreatePage } from '../item-create/item-create';
 import { ItemDetailPage } from '../item-detail/item-detail';
@@ -9,6 +9,15 @@ import { Items } from '../../providers/providers';
 import { Item } from '../../models/item';
 
 import { User } from '../../providers/providers';
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsEvent,
+  LatLng,
+  CameraPosition,
+  MarkerOptions,
+  Marker
+ } from '@ionic-native/google-maps';
 
 @Component({
   selector: 'page-list-master',
@@ -16,9 +25,16 @@ import { User } from '../../providers/providers';
 })
 export class ListMasterPage {
   currentItems: Item[];
+  position: CameraPosition;
+  public lat: any = 51.678418;
+  public lng: any = 7.809007;
 
-  constructor(public navCtrl: NavController, public items: Items, public modalCtrl: ModalController, public user: User) {
+  @ViewChild('map') map;
+
+  constructor(public navCtrl: NavController, public items: Items, public modalCtrl: ModalController,
+     public user: User, private googleMaps: GoogleMaps) {
     this.currentItems = this.items.query();
+
   }
 
   /**
@@ -26,6 +42,52 @@ export class ListMasterPage {
    */
   ionViewDidLoad() {
   }
+
+  // Load map only after view is initialized
+ngAfterViewInit() {
+  this.loadMap();
+ }
+
+  loadMap() {
+    // create a new map by passing HTMLElement
+    let element: HTMLElement = document.getElementById('map');
+    
+      let map: GoogleMap = this.googleMaps.create(element);
+    
+      // listen to MAP_READY event
+      // You must wait for this event to fire before adding something to the map or modifying it in anyway
+      map.one(GoogleMapsEvent.MAP_READY).then(
+        () => {
+          console.log('Map is ready!');
+          // Now you can add elements to the map like the marker
+        }
+      );
+      /*
+      // create CameraPosition
+      let position: CameraPosition = {
+        target: {
+          lat: 43.0741904,
+          lng: -89.3809802
+        },
+        zoom: 18,
+        tilt: 30
+      };
+    
+      // move the map's camera to position
+      map.moveCamera(position);
+    
+      // create new marker
+      let markerOptions: MarkerOptions = {
+        position: ionic,
+        title: 'Ionic'
+      };
+    
+      const marker: Marker = map.addMarker(markerOptions)
+        .then((marker: Marker) => {
+          marker.showInfoWindow();
+        });
+      */
+    }
 
   /**
    * Prompt the user to add a new item. This shows our ItemCreatePage in a
